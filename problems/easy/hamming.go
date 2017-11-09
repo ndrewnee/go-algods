54c90afc4988ff094c314b2fb3a217d1972bdbd0
Andrew Nee <ndrewnee@gmail.com>
2017-11-10 00:07:27 +0300

    Added solution for Hamming distance

--- problems/easy/hamming.go
+++ problems/easy/hamming.go
@@ -1,9 +1,64 @@
 package easy
 
+import (
+	"strconv"
+	"strings"
+)
+
 // HammingDistance calculates the Hamming distace.
 // The Hamming distance between two integers is the number of positions
 // at which the corresponding bits are different.
-// TODO Implement
 func HammingDistance(x, y uint64) uint64 {
-	return 0
+	var distance uint64
+	// XOR of two numbers
+	// 6(110) and 0(000) gives 6(110)
+	value := x ^ y
+	// count the number of bits set
+	for value != 0 {
+		// A bit is set, so increment the count and clear the bit
+		distance++
+		// counting bits by algorihm of Peter Wegner
+		// http://dl.acm.org/citation.cfm?id=367236.367286
+		// step1: 6(110) & 5(101) = 4(100) (distance=1)
+		// step2: 4(100) & 3(011) = 0(000) (distance=2)
+		value &= value - 1
+	}
+	// Return the number of differing bits
+	return distance
+}
+
+// HammingDistanceDummy is unoptimised dummy version
+// to calculate Hamming distance
+func HammingDistanceDummy(x, y uint64) uint64 {
+	// binary represantation of 1st number
+	s1 := strconv.FormatUint(x, 2)
+	// binary represantation of 2nd number
+	s2 := strconv.FormatUint(y, 2)
+	// completes strings if they have not equal lengths
+	// for example, s1="1001", s2="111000"
+	// then after func s1=001001, s2=111000
+	s1, s2 = completeStrings(s1, s2)
+	// algorithm is easy, compare each symbol in both strings
+	// if they are not equal then increment distance
+	var distance uint64
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			distance++
+		}
+	}
+
+	return distance
+}
+
+// completeStrings adds some "0" (difference between length of
+// two numbers) before number that has less digits in binary string represantion
+func completeStrings(s1, s2 string) (string, string) {
+	diff := len(s1) - len(s2)
+	if diff > 0 {
+		s2 = strings.Repeat("0", diff) + s2
+	} else {
+		s1 = strings.Repeat("0", -diff) + s1
+	}
+
+	return s1, s2
 }
--- problems/easy/hamming_test.go
+++ problems/easy/hamming_test.go
@@ -16,19 +16,68 @@ func TestHammingDistance(t *testing.T) {
 		args args
 		want uint64
 	}{
-		{
-			"Valid",
-			args{
-				151,
-				180,
-			},
-			3,
-		},
+		{"1", args{0, 1}, 1},
+		{"2", args{0, 0}, 0},
+		{"3", args{6, 0}, 2},
+		{"4", args{65280, 255}, 16},
+		{"5", args{2, 0}, 1},
+		{"6", args{4, 5}, 1},
+		{"7", args{127, 126}, 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := HammingDistance(tt.args.x, tt.args.y)
+			dummy := HammingDistanceDummy(tt.args.x, tt.args.y)
 			assert.Equal(t, tt.want, got)
+			assert.Equal(t, tt.want, dummy)
+		})
+	}
+}
+
+func Test_completeStrings(t *testing.T) {
+	type args struct {
+		s1 string
+		s2 string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 string
+	}{
+		{
+			"1st > 2nd",
+			args{
+				"100101101",
+				"1011",
+			},
+			"100101101",
+			"000001011",
+		},
+		{
+			"1st < 2nd",
+			args{
+				"1111",
+				"101101101",
+			},
+			"000001111",
+			"101101101",
+		},
+		{
+			"1st == 2nd",
+			args{
+				"111111111",
+				"101101101",
+			},
+			"111111111",
+			"101101101",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := completeStrings(tt.args.s1, tt.args.s2)
+			assert.Equal(t, tt.want, got)
+			assert.Equal(t, tt.want1, got1)
 		})
 	}
 }
