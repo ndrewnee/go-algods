d435e42b0e24126cd329751e7ecf8fbbdefd5fe3
Andrew Nee <ndrewnee@gmail.com>
2018-11-11 01:15:23 +0300

    Added test for FindMaxWindows, removed redundant MinQueue1

--- exercises/max_win_test.go
+++ exercises/max_win_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFindMinWindows(t *testing.T) {
+func TestFindMaxWindows(t *testing.T) {
 	type args struct {
 		numbers    []int
 		windowSize int
@@ -28,41 +28,41 @@ func TestFindMinWindows(t *testing.T) {
 			want: []int{9, 2, 1, 3, 5, 21, 15, 6},
 		},
 		{
-			name: "should return one min because windowSize is greather than length",
+			name: "should return one max because windowSize is greather than length",
 			args: args{
 				numbers:    []int{9, 2, 1, 3, 5, 21, 15, 6},
 				windowSize: 20,
 			},
-			want: []int{1},
+			want: []int{21},
 		},
 		{
-			name: "should return one min because windowSize is 0",
+			name: "should return one max because windowSize is 0",
 			args: args{
 				numbers:    []int{9, 2, 1, 3, 5, 21, 15, 6},
 				windowSize: 0,
 			},
-			want: []int{1},
+			want: []int{21},
 		},
 		{
-			name: "should return valid min windows #1",
+			name: "should return valid max windows #1",
 			args: args{
 				numbers:    []int{9, 2, 1, 3, 5, 21, 15, 6},
 				windowSize: 3,
 			},
-			want: []int{1, 1, 1, 3, 5, 6},
+			want: []int{9, 3, 5, 21, 21, 21},
 		},
 		{
-			name: "should return valid min windows #2",
+			name: "should return valid max windows #2",
 			args: args{
 				numbers:    []int{5, 1, 3, 2, 4, 6, 1, 7, 3, 2, 8},
 				windowSize: 3,
 			},
-			want: []int{1, 1, 2, 2, 1, 1, 1, 2, 2},
+			want: []int{5, 3, 4, 6, 6, 7, 7, 7, 8},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := FindMinWindows(tt.args.numbers, tt.args.windowSize)
+			got := FindMaxWindows(tt.args.numbers, tt.args.windowSize)
 			assert.Equal(t, tt.want, got)
 		})
 	}
--- exercises/min_win.go
+++ exercises/min_win.go
@@ -1,12 +1,8 @@
 package exercises
 
-type MinQueue interface {
-	Min() int
-	Push(value int)
-	Pop(value int)
-}
+import "github.com/ndrewnee/go-algods/ds/stack"
 
-func findMinWindows(minQueue MinQueue, numbers []int, windowSize int) []int {
+func FindMinWindows(numbers []int, windowSize int) []int {
 	length := len(numbers)
 	if length == 0 {
 		return nil
@@ -20,6 +16,7 @@ func findMinWindows(minQueue MinQueue, numbers []int, windowSize int) []int {
 		return []int{findMin(numbers)}
 	}
 
+	minQueue := NewMinQueue()
 	for i := 0; i < windowSize; i++ {
 		minQueue.Push(numbers[i])
 	}
@@ -43,4 +40,77 @@ func findMin(numbers []int) int {
 	}
 
 	return min
+}
+
+type (
+	PairMin struct {
+		Value int
+		Min   int
+	}
+
+	MinQueue struct {
+		stack1 stack.Stack
+		stack2 stack.Stack
+	}
+)
+
+func NewMinQueue() *MinQueue {
+	return &MinQueue{stack1: stack.NewSliceStack(), stack2: stack.NewSliceStack()}
+}
+
+func (mq *MinQueue) Min() int {
+	if mq.stack1.Empty() {
+		return getMin(mq.stack2.Top())
+	}
+
+	if mq.stack2.Empty() {
+		return getMin(mq.stack1.Top())
+	}
+
+	min1 := getMin(mq.stack1.Top())
+	min2 := getMin(mq.stack2.Top())
+
+	return minOf(min1, min2)
+}
+
+func (mq *MinQueue) Push(value int) {
+	var min int
+	if mq.stack1.Empty() {
+		min = value
+	} else {
+		min = minOf(value, getMin(mq.stack1.Top()))
+	}
+
+	mq.stack1.Push(PairMin{Value: value, Min: min})
+}
+
+//nolint: unparam
+func (mq *MinQueue) Pop(value int) {
+	defer mq.stack2.Pop()
+	if !mq.stack2.Empty() {
+		return
+	}
+
+	for !mq.stack1.Empty() {
+		value := mq.stack1.Pop().(PairMin).Value
+		var min int
+		if mq.stack2.Empty() {
+			min = value
+		} else {
+			min = minOf(value, getMin(mq.stack2.Top()))
+		}
+		mq.stack2.Push(PairMin{Value: value, Min: min})
+	}
+}
+
+func minOf(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func getMin(value interface{}) int {
+	return value.(PairMin).Min
 }
