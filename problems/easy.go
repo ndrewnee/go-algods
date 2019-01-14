af9378a43aa964b0b43482fc44ebffe9642c8e51
Andrew Nee <ndrewnee@gmail.com>
2019-01-14 22:26:57 +0500

    Added solution for roman to integer

--- problems/easy.go
+++ problems/easy.go
@@ -158,3 +158,47 @@ func isSelfDiv(num int) bool {
 
 	return true
 }
+
+// https://leetcode.com/problems/roman-to-integer/
+func romanToInt(s string) int {
+	length := len(s)
+	if length == 0 {
+		return 0
+	}
+
+	result := getRomanValue(s[length-1])
+
+	for i := length - 2; i >= 0; i-- {
+		value := getRomanValue(s[i])
+
+		if value < getRomanValue(s[i+1]) {
+			result -= value
+			continue
+		}
+
+		result += value
+	}
+
+	return result
+}
+
+func getRomanValue(b byte) int {
+	switch b {
+	case 'I':
+		return 1
+	case 'V':
+		return 5
+	case 'X':
+		return 10
+	case 'L':
+		return 50
+	case 'C':
+		return 100
+	case 'D':
+		return 500
+	case 'M':
+		return 1000
+	default:
+		return 0
+	}
+}
