40bde79b912445556809a69a2230e9ef20c81288
Andrew Nee <ndrewnee@gmail.com>
2019-01-15 10:07:00 +0500

    Added solution for merging two sorted lists

--- problems/easy.go
+++ problems/easy.go
@@ -229,3 +229,42 @@ func isValid(s string) bool {
 
 	return len(stack) == 0
 }
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// https://leetcode.com/problems/merge-two-sorted-lists/
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	var root ListNode
+	tail := &root
+
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			tail.Next = l1
+			l1 = l1.Next
+		} else {
+			tail.Next = l2
+			l2 = l2.Next
+		}
+
+		tail = tail.Next
+	}
+
+	if l1 != nil {
+		tail.Next = l1
+	} else {
+		tail.Next = l2
+	}
+
+	return root.Next
+}
